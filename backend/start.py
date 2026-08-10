ada67b9cd5d54a13a57a94882afe47a263202a31
Syed Jawad Ali <syedjawadali92@gmail.com>
2026-08-10 15:47:55 +0500

    Changes

--- backend/start.py
+++ backend/start.py
@@ -1,10 +1,3 @@
-# import uvicorn
-
-# if __name__ == "__main__":
-#     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
-
-
-
 # start.py
 from app.main import app
 import uvicorn
@@ -17,3 +10,9 @@ if __name__ == "__main__":
         reload=True,
         log_level="debug"  # Get more detailed logs
     )
+
+
+
+
+
+    
